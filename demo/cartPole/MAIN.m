01adb4f3675ff88c38f93ce2a261cfbcdd2b5858
MatthewPeterKelly <matthew.kelly2@gmail.com>
2015-11-23 14:50:02 -0500

    Solved trajectory optimization problem

---
+++ demo/cartPole/MAIN.m
@@ -0,0 +1,89 @@
+% MAIN.m 
+%
+% Solve the cart-pole swing-up problem
+
+clc; clear;
+
+p.m1 = 1.0;  % (kg) Cart mass
+p.m2 = 0.3;  % (kg) pole mass
+p.g = 9.81;  % (m/s^2) gravity 
+p.l = 0.5;   % (m) pendulum (pole) length 
+
+dist = 1.0;  %How far must the cart translate during its swing-up
+maxForce = 100;  %Maximum actuator forces
+duration = 3;
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+%                     Set up function handles                             %
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+problem.func.dynamics = @(t,x,u)( cartPoleDynamics(x,u,p) );
+problem.func.pathObj = @(t,x,u)( u.^2 );  %Force-squared cost function
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+%                     Set up problem bounds                               %
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+problem.bounds.initialTime.low = 0;
+problem.bounds.initialTime.upp = 0;
+problem.bounds.finalTime.low = duration;
+problem.bounds.finalTime.upp = duration;
+
+problem.bounds.initialState.low = zeros(4,1);
+problem.bounds.initialState.upp = zeros(4,1);
+problem.bounds.finalState.low = [dist;pi;0;0];
+problem.bounds.finalState.upp = [dist;pi;0;0];
+
+problem.bounds.state.low = [-2*dist;-2*pi;-inf;-inf];
+problem.bounds.state.upp = [2*dist;2*pi;inf;inf];
+
+problem.bounds.control.low = -maxForce;
+problem.bounds.control.upp = maxForce;
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+%                    Initial guess at trajectory                          %
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+problem.guess.time = [0,duration];
+problem.guess.state = [problem.bounds.initialState.low, problem.bounds.finalState.low];
+problem.guess.control = [0,0];
+
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+%                         Solver options                                  %
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+problem.options.nlpOpt = optimset(...
+    'Display','iter',...
+    'MaxFunEvals',1e5);
+
+problem.options.method = 'hermiteSimpson';
+
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+%                            Solve!                                  %
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+soln = trajOpt(problem);
+
+
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+%                        Display Solution                                 %
+%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
+
+%%%% Unpack the simulation
+t = linspace(soln.grid.time(1), soln.grid.time(end), 200);
+z = soln.interp.state(t);
+u = soln.interp.control(t);
+
+%%%% Plots:
+figure(1); clf;
+plotPendulumCart(t,z,u,p);
+
+
+%%%% Draw Trajectory:
+[p1,p2] = cartPoleKinematics(z,p);
+
+figure(2); clf; 
+nFrame = 9;  %Number of frames to draw
+drawCartPoleTraj(t,p1,p2,nFrame);
